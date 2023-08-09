4d9c30ad7320fba3d3143a5f420d3873abbf83dd
Mrfantasticc <ajay.electromania@gmail.com>
2023-08-09 13:33:49 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly rwOnly = new RWOnly();
+    //System.out.println(rwOnly.accountBalance(50)); //accountBalance(int) has private access in com.driver.RWOnly
+    rwOnly.setAccountBalance(1000);
+    System.out.println(rwOnly.getAccountBalance());
+  }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private int accountBalance;
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(int accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
 }
